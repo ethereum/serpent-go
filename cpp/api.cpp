9c977ab11be03070535e6c177bda515ef62cd550
obscuren <geffobscura@gmail.com>
2014-06-21 01:56:08 +0200

    Compile to hex

--- cpp/api.cpp
+++ cpp/api.cpp
@@ -2,14 +2,20 @@
 #include "serpent/funcs.h"
 
 #include "api.h"
+#include <iostream>
 
 const char *compileGo(char *code, int *err)
 {
     try {
-        return compile(std::string(code)).c_str();
+        std::string c = binToHex(compile(std::string(code)));
+
+        return c.c_str();
     }
     catch(std::string &error) {
         *err = 1;
         return error.c_str();
+    }
+    catch(...) {
+        return "Unknown error";
     }
 }
